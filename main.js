66007ce49d2c6ef343ed0e2e0376318a9c382980
SreeharshaReddy <harshapriya2353@gmail.com>
2016-03-31 14:46:38 -0700

    modified code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var square = require('lib.js').square;
-var diag = require('lib.js').diag;
+var square = require('./lib.js').square;
+var diag = require('./lib.js').diag;
 console.log(square(11)); // 121
 console.log(diag(4, 3)); // 5
