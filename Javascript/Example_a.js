865a9c609efffe12991bb044eac058d4f9b0a43b
SreeharshaReddy <harshapriya2353@gmail.com>
2016-03-31 16:19:15 -0700

    commit code

--- Javascript/Example_a.js
+++ Javascript/Example_a.js
@@ -1,7 +1,6 @@
-module.export const sqrt = Math.sqrt;
-    export function square(x) {
-        return x * x;
-    }
-module.export function diag(x, y) {
-        return sqrt(square(x) + square(y));
-    }
+module.exports={
+hello:function(){
+return hello
+area.function
+}
+}
