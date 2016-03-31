c059ffcfa2052c9c4cd7bdb7168d3501c98bc20c
SreeharshaReddy <harshapriya2353@gmail.com>
2016-03-31 14:58:19 -0700

    commit

--- a.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-hello:function(){
-return hello
-area.function
-}
-}
--- b.js
+++
@@ -1,4 +0,0 @@
-var a=require('./a.js');
-console.log(a.hello());
-console.log('area of circle' + a.area(10));
-console.log('perimeter of circle' + a.perimeter(10));
